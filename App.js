c8eae4d95a837f43f29beea0bef4586070758b60
Alex Markin <alexndr86@gmail.com>
2020-10-30 13:19:02 +0300

    Add android safe area

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+  TextInputComponent,
+} from "react-native";
+import GlobalStyles from "./GlobalStyles";
 
 export default function App() {
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [gigs, setGigs] = useState([
+    {
+      description: "Freelance job with Qazi",
+      amount: 499.99,
+    },
+  ]);
+
+  const addGig = () =>
+    setGigs([
+      ...gigs,
+      {
+        description: description,
+        amount: amount,
+      },
+    ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={GlobalStyles.androidSafeArea}>
+      <View>
+        <Text style={styles.titleText}>React Native App for Freelance</Text>
+      </View>
+      <TextInput
+        style={styles.gigInput}
+        value={description}
+        placeholder="Enter a description"
+        onChangeText={(text) => setDescription(text)}
+      />
+      <TextInput
+        style={styles.gigInput}
+        value={amount}
+        placeholder="Enter the amount you made in $ (USD)"
+        onChangeText={(text) => setInput(text)}
+        keyboardType="number-pad"
+      />
+      <Button title="Add Gig 🚀" onPress={addGig} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  gigInput: {
+    margin: 20,
+    height: 40,
+    borderColor: "red",
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 30,
   },
 });
